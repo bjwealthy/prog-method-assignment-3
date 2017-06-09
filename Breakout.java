2d959e3c3e646198c523cb3c0eb1fc934bb39a6a
S <s>
2017-06-09 13:45:07 -0700

    {"types":"C","runs":18}

--- Breakout.java
+++ Breakout.java
@@ -70,10 +70,11 @@ private void createBricks(int n, double cx, double cy){
 		for(int col = 0; col < 10; col++){
 			//to get the x- and y-coordinates of the starting brick
 			//i.e the one at the centre
-			double x = cx - (NBRICKS_PER_ROW*BRICK_WIDTH)/2 - (NBRICKS_PER_ROW - 1)*BRICK_SEP)/2  + col*BRICK_WIDTH + col*BRICK_SEP)/2 + BRICK_WIDTH + col*BRICK_SEP;
+			double x = cx - (NBRICKS_PER_ROW*BRICK_WIDTH)/2 - (NBRICKS_PER_ROW - 1)*BRICK_SEP/2  + col*BRICK_WIDTH + col*BRICK_SEP)/2 + BRICK_WIDTH + col*BRICK_SEP;
 			double y = cy + row*BRICK_HEIGHT + row*BRICK_SEP;
 			
-		GRect bri = new GRect(x, y, BRICK_WIDTH< BRICK_HEIGHT);
+		private GRect bri;
+		bri = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
 		add(bri);
 		
 		//painting bricks
@@ -83,13 +84,116 @@ private void createBricks(int n, double cx, double cy){
 			bri.setColor(Color.ORANGE);
 		for (row = 4; row < 6; row++)
 			bri.setColor(Color.YELLOW);
-		for(row = 6; row < 7; row++)
-			bri.setC
+		for(row = 6; row < 8; row++)
+			bri.setColor(Color.GREEN);
+		for(row = 7; row < 10; row++ );
 		
-		
-			
 			
 		}
 	}
 }
+
+//to add and crate paddles
+private GRect paddle; //paddle as an instance variable
+
+private void makePaddl(){
+	double x = getWidth()/2 - PADDLE_WIDTH/2; //THIS IS WHRE PADDLe starts from x = 0;
+	double y = getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT; //BASELINE OF PADDLE
+	paddle = new GRect(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
+	paddle.setFilled(true);
+	
+	addMouseListeners(); //to listen foe mouse events
+	add(paddle);	
 }
+
+public void mouseMoved(MouseEvent e){
+	if(e.getX() < (getWidth() - (PADDLE_WIDTH/2))) && (e.getX() > (PADDLE_WIDTH/2)))
+paddle.setLocation(e.getX() - PADDLE_WIDTH/2, getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT);
+}
+}
+
+//drawing and adding ball
+private GOval ball;  //ball is declared as an instance variable 
+
+private void drawBall(){
+	ball = new GOval(x, y, BALL_RADIUS);
+	ball.setFilled(true);
+	double x = getWidth()/2 - BALL_RADIUS;
+	double y = getHeight()/2 - BALL_RADIUS;
+	add(ball);
+}
+
+private void play(){
+	getVelocityOfBall();
+	waitForClick();
+	while(true){
+		moveBall();
+		
+		if(ball.getY() >= getHeight()){
+			break;
+		}
+	}
+}
+
+private void moveBall(){
+	ball.move(vx, vy);
+	
+	if((ball.getX() - vy <= 0 && vx < 0)){
+		vx = -vy;
+}
+	
+	GObject collider = getCollidingObject();
+	if(collider == paddle){
+		if(ball.getY() >= getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT - BALL_RADIUS*2 && ball.getY() < getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT - BALL_RADIUS*2 + 4){
+			vy = -vy;
+		}
+	}
+	
+	else if(collider != null){
+		remove(collider);
+		brickCounter--;
+		vy = -vy;
+	}
+	pause(DELAY);
+}
+
+private GObject getCollidingObject(){
+	if(getElentAt(ball.get(), ball.getY()) != null){
+		return  getElementAt(ball.getX(), ball.getY());
+    }
+  else if (getElementAt( (ball.getX() + BALL_RADIUS*2), ball.getY()) != null ){
+       return getElementAt(ball.getX() + BALL_RADIUS*2, ball.getY());
+    }
+  else if(getElementAt(ball.getX(), (ball.getY() + BALL_RADIUS*2)) != null ){
+       return getElementAt(ball.getX(), ball.getY() + BALL_RADIUS*2);
+    }
+  else if(getElementAt((ball.getX() + BALL_RADIUS*2), (ball.getY() + BALL_RADIUS*2)) != null ){
+       return getElementAt(ball.getX() + BALL_RADIUS*2, ball.getY() + BALL_RADIUS*2);
+    }
+  //need to return null if there are no objects present
+  else{
+       return null;
+    }
+  
+}
+
+private void printGameOver() {
+  GLabel gameOver = new GLabel ("Game Over", getWidth()/2, getHeight()/2);
+  gameOver.move(-gameOver.getWidth()/2, -gameOver.getHeight());
+  gameOver.setColor(Color.RED);
+  add (gameOver);
+}
+
+private int brickCounter = 100;
+
+private void printWinner() {
+  GLabel Winner = new GLabel ("Winner!!", getWidth()/2, getHeight()/2);
+  Winner.move(-Winner.getWidth()/2, -Winner.getHeight());
+  Winner.setColor(Color.RED);
+  add (Winner);
+
+		
+	}
+	
+}
+
